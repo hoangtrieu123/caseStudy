bd679181a7e89b51a7c432c55aab0f1481291c3f
hoangtrieu123 <104626250+hoangtrieu123@users.noreply.github.com>
2022-05-29 18:08:31 +0700

    Add files via upload

--- qlhs.js
+++ qlhs.js
@@ -1,7 +1,8 @@
 var data = []
 clear()
+let list = data
+
 function checkID(id) {
-    var list = data
     for (let i = 0; i < list.length; i++) {
         if (list[i].id === id) {
             return false
@@ -32,14 +33,18 @@ function addData() {
     console.log(data)
 
 }
-function clear(){
+
+function clear() {
     document.getElementById("id").value = ""
     document.getElementById("name").value = ""
     document.getElementById("address").value = ""
     document.getElementById("email").value = ""
 }
+
 function editData(id) {
-    var list = data
+
+    document.getElementById("confirm").innerHTML = "<button id=addData onClick=addData()>Thêm</button>" +
+        "&nbsp;"+"<td><button onclick=confirmData('${list[i].id}')>Xác nhận sửa</button>"
     for (let i = 0; i < list.length; i++) {
         if (list[i].id == id) {
             document.getElementById("id").value = list[i].id
@@ -50,40 +55,54 @@ function editData(id) {
     }
 }
 
+function confirmData(id) {
+
+    for (let i = 0; i < list.length; i++) {
+
+            list[i].id = document.getElementById("id").value
+            list[i].name = document.getElementById("name").value
+            list[i].address = document.getElementById("address").value
+            list[i].email = document.getElementById("email").value
+    }
+    viewData()
+}
+
 function deleteData(id) {
-    var list = data
+
     let choice = confirm("Bạn có muốn xóa không ?")
     console.log(list)
     for (let i = 0; i < list.length; i++) {
-        console.log(id, list[i].id)
         if (list[i].id == id && choice === true) {
             list.splice(i, 1)
-
             viewData()
         }
     }
 }
 
 function viewData() {
-    var list = this.data
+
     var table = '<table cellpadding="5" border="1">' +
         '<tr>' +
         '<th>Mã học sinh</th>' +
         '<th>Tên học sinh</th>' +
         '<th>Địa chỉ</th>' +
         '<th>Email</th>' +
-        '<th>Sửa/Xóa</th>'+
-    '</tr>' +
-    '</th>' + '<tbody>'
+        '<th>Sửa/Xóa</th>' +
+        '</tr>' +
+        '</th>' + '<tbody>'
     for (let i = 0; i < list.length; i++) {
         table += '<tr>'
         table += '<td>' + list[i].id + '</td>'
         table += '<td>' + list[i].name + '</td>'
         table += '<td>' + list[i].address + '</td>'
         table += '<td>' + list[i].email + '</td>'
-        table += `<td><button onclick="editData('${list[i].id}')">Sửa</button>&nbsp;<button onclick="deleteData('${list[i].id}')">Xóa</button></td>`
+        table += `<td>
+<button onclick="editData('${list[i].id}')">Sửa</button>
+<button onclick="deleteData('${list[i].id}')">Xóa</button>
+</td>`
         table += '</tr>'
     }
     table += '</tbody></table>'
     document.getElementById("result").innerHTML = table
+
 }
